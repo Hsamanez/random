2d50637cebf71c4320c64324121a83a296d9df2a
Hugo Samanez Atoche <hugo.samanez@gmail.com>
2018-08-09 00:42:12 -0500

    version 2

--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 // Math.random() devuelve un nuemro aleatorio entre 0 y 1
 var numero = Math.random();
+var str = 'Mayor que 0,5';
 
 if (numero <= 0.5) {
-	console.log('\n' + numero + 'MENOR que 0,5 \n');
+	str = ' Menor que 0,5';
 }
-else {
-	console.log('\n' + numero + 'MAYOR que 0,5 \n');
-}
+
+console.log('\n' + numero +  str + \n');
